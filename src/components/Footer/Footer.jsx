55839d30e4b4041f70b4cee1eb35984b6cbcb3fa
Rohyer <guilhermerl.dev@gmail.com>
2023-09-17 18:04:53 -0300

    Develop Footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Todos os direitos reservados.</p>
+    </footer>
+  );
+};
+
+export default Footer;
